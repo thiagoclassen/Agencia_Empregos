cf7d26a175f44030719a5049e38fc17b2dbd0a36
Thiago Classen <tgclassen.dev@gmail.com>
2019-10-17 00:19:21 -0300

    first commit

---
+++ Servidor/src/Interfaces/InterfaceServ.java
@@ -0,0 +1,8 @@
+package Interfaces;
+
+import java.rmi.Remote;
+import java.rmi.RemoteException;
+
+public interface InterfaceServ extends Remote {
+    public void chamar(String msg, InterfaceCli interfaceCli) throws RemoteException;
+}
---
+++ Servidor/src/Objects/Vaga.java
@@ -0,0 +1,63 @@
+package Objects;
+
+public class Vaga {
+    private String empresa;
+    private String contato;
+    private String area;
+    private int cargaHoraria;
+    private int salario;
+
+    public Vaga(){  }
+
+    public Vaga(String empresa, String contato, String area, int cargaHoraria, int salario){
+        this.empresa = empresa;
+        this.contato = contato;
+        this.area = area;
+        this.cargaHoraria = cargaHoraria;
+        this.salario = salario;
+    }
+    
+    //#region Getters and Setters
+    public int getSalario() {
+        return salario;
+    }
+
+    public void setSalario(int salario) {
+        this.salario = salario;
+    }
+
+    public int getCargaHoraria() {
+        return cargaHoraria;
+    }
+
+    public void setCargaHoraria(int cargaHoraria) {
+        this.cargaHoraria = cargaHoraria;
+    }
+
+    public String getArea() {
+        return area;
+    }
+
+    public void setArea(String area) {
+        this.area = area;
+    }
+
+    public String getContato() {
+        return contato;
+    }
+
+    public void setContato(String contato) {
+        this.contato = contato;
+    }
+
+    public String getEmpresa() {
+        return empresa;
+    }
+
+    public void setEmpresa(String empresa) {
+        this.empresa = empresa;
+    }
+
+    //#endregion
+    
+}
